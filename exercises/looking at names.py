c6c9240a2403436c03b9c9051b43681d2a3b1c1b
Ian McAllister <ian@mcallisternevins.com>
2019-01-10 18:54:11 -0500

    checkpoint

---
+++ exercises/looking at names.py
@@ -0,0 +1,8 @@
+# 1. write a program to ask user for his name and then
+# tell him how letters in his name
+
+
+print ("hello")
+player_name = input ("what is your name")
+print ("hi", player_name)
+print(len(player_name), "letters are in your name")
--- hangman/hangman.py
+++ hangman/hangman.py
@@ -11,15 +11,23 @@ secret_word = hangman_words[random.randint(0,852)]
 print ("welcome to hangman")
 player_name = input("What is your name? ")
 print ("hello", player_name)
-#print ("the secret word is",secret_word)
+print ("Number of letters in the secret word is",len(secret_word))
 number_guesses = 0
-
+progress = []
+for i in range(len(secret_word)):
+    progress.append('_')
+    
 while number_guesses < 11:
+    print("Word is", progress)
     player_guess = input ("what's your guess? ")
     guess = player_guess
     for position, letter in enumerate(secret_word):
-       if letter == guess: 
-           print('HEY YEAH GOT IT at position', position)
-       else:
-           print('SORRY SUCKER. its not at position', position)
+       if letter == guess:
+           progress[position] = guess
     number_guesses = number_guesses + 1
+    if '_' in progress:
+        print("Sorry you're done")
+    else:
+        print ("Great job, you finished!")
+        break
+    
