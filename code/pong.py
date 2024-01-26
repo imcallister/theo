4face20a57f7e1dea464ca478efbd6aff323d168
Ian McAllister <ianmcallister@ians-air.lan>
2024-01-26 08:28:19 -0500

    work in class 1/25

--- code/pong.py
+++ code/pong.py
@@ -1,5 +1,9 @@
 import pygame as pg
 
+SCREEN = pg.display.set_mode((1280, 960), pg.FULLSCREEN)
+
+screen_boundary = SCREEN.get_rect()
+
 class Paddle(pg.sprite.Sprite):
 
     speed = 0
@@ -12,6 +16,19 @@ class Paddle(pg.sprite.Sprite):
         self.image.fill(color)
         self.rect = self.image.get_rect()
 
+               
+
+class Ball(pg.sprite.Sprite):
+
+    speed = 0
+
+    def __init__(self, color, width, height):
+
+        pg.sprite.Sprite.__init__(self)
+
+        self.image = pg.Surface([width, height])
+        self.image.fill(color)
+        self.rect = self.image.get_rect()
 
 
 BLACK = (  0,  0,  0)
@@ -26,12 +43,16 @@ BLUE = (255, 255, 255)
 
 game_on = True
 
+
+
 def main():
     pg.init()
 
-    SCREEN = pg.display.set_mode((1280, 960), pg.FULLSCREEN)
+    lkey_down = False
 
-    paddle1 = Paddle(WHITE, 500, 150)    
+    rkey_down = False
+
+    paddle1 = Paddle(WHITE, 110, 15)    
 
     clock = pg.time.Clock()
 
@@ -39,14 +60,62 @@ def main():
     all_sprites_list.add(paddle1)
 
 
-    paddle1.rect.x = 640
-    paddle1.rect.y = 480
+    paddle1.rect.x = 763
+    paddle1.rect.y = 1025
+
+    ball1 = Ball(WHITE, 15, 15)
+
+    ball1.rect.x = 800
+    ball1.rect.y = 550
+
+    all_sprites_list.add(ball1)
+    
+    screen_boundary = SCREEN.get_rect()
+    screen_boundary.clamp(paddle1)
 
     while game_on:
+        if lkey_down == True:
+            move_left()
+        if rkey_down == True:
+            move_right()
+
+        def move_left():
+            current_x = paddle1.rect.x
+        
+            paddle1.rect.x = current_x - 10
+        
+            SCREEN.fill(BLACK)
+
+            all_sprites_list.draw(SCREEN)
+
+        def move_right():
+            current_x = paddle1.rect.x
+                
+            paddle1.rect.x = current_x + 10
+                
+            SCREEN.fill(BLACK)
+
+            all_sprites_list.draw(SCREEN)     
+        
+       
         for event in pg.event.get():
+            
             if event.type == pg.KEYDOWN and event.key == pg.K_q:
                 return
             
+            if event.type == pg.KEYDOWN and event.key == pg.K_LEFT:
+                lkey_down = True
+
+            if event.type == pg.KEYUP and event.key == pg.K_LEFT:
+                lkey_down = False 
+
+            if event.type == pg.KEYDOWN and event.key == pg.K_RIGHT:
+                rkey_down = True
+
+            if event.type == pg.KEYUP and event.key == pg.K_RIGHT:
+                rkey_down = False 
+   
+    
         all_sprites_list.draw(SCREEN)
         pg.display.flip()
         clock.tick(40)
