d713933b17c13d479eea60bf4e7858901dba5186
Ian McAllister <ianmcallister@ians-air.lan>
2024-04-18 06:50:37 -0400

    palindromes

--- code/prime_numbers.py
+++ code/prime_numbers.py
@@ -1,3 +1,32 @@
 
+game_on = True
+def is_factor(k, n):
+    input_1 = int(n)
+    input_2 = int(k)
+    if input_1 % input_2 == 0:
+        return True
+    else:
+        return False
 
-input_number = input('Please enter a positive integer:  ')
+def is_prime(n):
+
+    for i in range(2,n):
+        if is_factor(i, n) == False:
+            pass
+        else:
+            return False
+    return True
+
+while game_on == True:
+    input_ = (input('Please enter a positive integer:  '))
+    if input_ == "q":
+        game_on = False
+    else:
+        input_num = int(input_)
+        if input_num < 2:
+            print("is neither")
+        else:
+            if (is_prime(input_num)) == True:
+                print("is prime")
+            else:
+                print("is composite")
